63e6fc0d407579bf17269751bff8a8dc575db2fe
Robert Forkel <xrotwang@googlemail.com>
2021-10-15 09:20:32 +0200

    Next iteration
    - keep BEAST annotations
    - nexus with trees has suffix .trees
    - added data

--- cldfbench_birchall_et_al2016.py
+++ cldfbench_birchall_et_al2016.py
@@ -1,5 +1,4 @@
 import gzip
-import shutil
 import pathlib
 
 import nexus
@@ -11,50 +10,21 @@ class Dataset(phlorest.Dataset):
     id = "birchall_et_al2016"
 
     def cmd_makecldf(self, args):
-        self.add_schema(args)
-        shutil.copy(self.raw_dir / 'source.bib', self.cldf_dir / 'sources.bib')
-        lids = self.add_taxa(args)
+        self.init(args)
 
-        f = nexus.NexusReader(self.raw_dir / 'relaxed-binary-simple.time.mcct.trees')
-        f.trees.detranslate()
-
-        assert len(f.trees.trees) == 1
-        summary = f.trees.trees[0]
-        phlorest.check_tree(summary, lids, args.log)
-
-        with phlorest.NexusFile(self.cldf_dir / 'summary.nex') as nex:
-            nex.append(summary)
-
-            args.writer.objects['trees.csv'].append(dict(
-                ID='summary',
-                Name=summary.name,
-                Nexus_File=nex.path.name,
-                rooted=summary.rooted,
-                type='summary',
-                method='Consensus tree from a bayesian analysis',
-                scaling=self.metadata.scaling,
-                Source=['Birchall_et_al2016'],
-            ))
+        with phlorest.NexusFile(self.cldf_dir / 'summary.trees') as nex:
+            f = nexus.NexusReader(self.raw_dir / 'relaxed-binary-simple.time.mcct.trees')
+            f.trees.detranslate()
+            assert len(f.trees.trees) == 1
+            self.add_tree(args, f.trees.trees[0], nex, 'summary', 'summary')
 
         with gzip.open(self.raw_dir / 'relaxed-binary-simple.time.trees.gz') as f:
-            posterior = f.read().decode('utf8')
-            posterior = self.run_nexus('--log-level WARNING trees -c -d 1-10000', posterior)
-            # FIXME: set random seed!?
             posterior = nexus.NexusReader.from_string(
-                self.run_nexus('trees --random-seed 12345 -t -n 1000', posterior))
+                self.sample(self.remove_burnin(f.read().decode('utf8'), 10000), detranslate=True))
 
-        with phlorest.NexusFile(self.cldf_dir / 'posterior.nex') as nex:
+        with phlorest.NexusFile(self.cldf_dir / 'posterior.trees') as nex:
             for i, tree in enumerate(posterior.trees.trees, start=1):
-                phlorest.check_tree(tree, lids, args.log)
-                nex.append(tree)
+                self.add_tree(
+                    args, tree, nex, 'posterior-{}'.format(i), 'sample')
 
-                args.writer.objects['trees.csv'].append(dict(
-                ID='posterior-{}'.format(i),
-                Name=tree.name,
-                Nexus_File=nex.path.name,
-                rooted=tree.rooted,
-                type='sample',
-                method='Sample from posterior of a bayesian analysis',
-                scaling=self.metadata.scaling,
-                Source=['Birchall_et_al2016'],
-            ))
+        self.add_data(args, self.raw_dir / 'Chapacuran_Swadesh207-2019-labelled.nex')
