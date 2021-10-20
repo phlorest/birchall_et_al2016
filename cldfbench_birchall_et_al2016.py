09deffd313b1b2a22c248326ac8aa1fb9cf0c958
Robert Forkel <xrotwang@googlemail.com>
2021-10-20 13:56:16 +0200

    upgrade phlorest

--- cldfbench_birchall_et_al2016.py
+++ cldfbench_birchall_et_al2016.py
@@ -1,8 +1,6 @@
-import gzip
 import pathlib
 
-import nexus
-from pydplace import phlorest
+import phlorest
 
 
 class Dataset(phlorest.Dataset):
@@ -13,18 +11,24 @@ class Dataset(phlorest.Dataset):
         self.init(args)
 
         with phlorest.NexusFile(self.cldf_dir / 'summary.trees') as nex:
-            f = nexus.NexusReader(self.raw_dir / 'relaxed-binary-simple.time.mcct.trees')
-            f.trees.detranslate()
-            assert len(f.trees.trees) == 1
-            self.add_tree(args, f.trees.trees[0], nex, 'summary', 'summary')
+            self.add_tree_from_nexus(
+                args,
+                self.raw_dir / 'relaxed-binary-simple.time.mcct.trees',
+                nex,
+                'summary',
+                'summary',
+                detranslate=True,
+            )
 
-        with gzip.open(self.raw_dir / 'relaxed-binary-simple.time.trees.gz') as f:
-            posterior = nexus.NexusReader.from_string(
-                self.sample(self.remove_burnin(f.read().decode('utf8'), 10000), detranslate=True))
+        posterior = self.sample(
+            self.remove_burnin(
+                self.read_gzipped_text(self.raw_dir / 'relaxed-binary-simple.time.trees.gz'),
+                10000),
+            detranslate=True,
+            as_nexus=True)
 
         with phlorest.NexusFile(self.cldf_dir / 'posterior.trees') as nex:
             for i, tree in enumerate(posterior.trees.trees, start=1):
-                self.add_tree(
-                    args, tree, nex, 'posterior-{}'.format(i), 'sample')
+                self.add_tree(args, tree, nex, 'posterior-{}'.format(i), 'sample')
 
         self.add_data(args, self.raw_dir / 'Chapacuran_Swadesh207-2019-labelled.nex')
