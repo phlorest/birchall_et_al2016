7ab32c44cbe4fd1e1c79751aa7fb078b8bf349ec
Robert Forkel <xrotwang@googlemail.com>
2021-11-25 15:08:54 +0100

    new phlorest API

--- cldfbench_birchall_et_al2016.py
+++ cldfbench_birchall_et_al2016.py
@@ -10,25 +10,26 @@ class Dataset(phlorest.Dataset):
     def cmd_makecldf(self, args):
         self.init(args)
 
-        with phlorest.NexusFile(self.cldf_dir / 'summary.trees') as nex:
-            self.add_tree_from_nexus(
-                args,
-                self.raw_dir / 'relaxed-binary-simple.time.mcct.trees',
-                nex,
-                'summary',
-                'summary',
-                detranslate=True,
-            )
+        args.writer.add_summary(
+            self.raw_dir.read_tree(
+                'relaxed-binary-simple.time.mcct.trees', detranslate=True),
+            self.metadata,
+            args.log)
 
         posterior = self.sample(
             self.remove_burnin(
-                self.read_gzipped_text(self.raw_dir / 'relaxed-binary-simple.time.trees.gz'),
+                self.raw_dir.read('relaxed-binary-simple.time.trees.gz'),
                 10000),
             detranslate=True,
             as_nexus=True)
 
-        with phlorest.NexusFile(self.cldf_dir / 'posterior.trees') as nex:
-            for i, tree in enumerate(posterior.trees.trees, start=1):
-                self.add_tree(args, tree, nex, 'posterior-{}'.format(i), 'sample')
+        args.writer.add_posterior(
+            posterior.trees.trees, 
+            self.metadata, 
+            args.log)
 
-        self.add_data(args, self.raw_dir / 'Chapacuran_Swadesh207-2019-labelled.nex')
+        args.writer.add_data(
+            self.raw_dir.read_nexus('Chapacuran_Swadesh207-2019-labelled.nex'),
+            self.characters, 
+            args.log)
+
