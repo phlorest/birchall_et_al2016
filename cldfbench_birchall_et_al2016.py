3ebfeedc582dc08d4714bdc667e70099f7daaa19
SimonGreenhill <simon@simon.net.nz>
2022-04-21 21:59:46 +1200

    update

--- cldfbench_birchall_et_al2016.py
+++ cldfbench_birchall_et_al2016.py
@@ -9,24 +9,16 @@ class Dataset(phlorest.Dataset):
 
     def cmd_makecldf(self, args):
         self.init(args)
-
         args.writer.add_summary(
             self.raw_dir.read_tree(
                 'relaxed-binary-simple.time.mcct.trees', detranslate=True),
             self.metadata,
             args.log)
 
-        posterior = self.sample(
-            self.remove_burnin(
-                self.raw_dir.read('relaxed-binary-simple.time.trees.gz'),
-                10000),
-            detranslate=True,
-            as_nexus=True)
-
-        args.writer.add_posterior(
-            posterior.trees.trees, 
-            self.metadata, 
-            args.log)
+        posterior = self.raw_dir.read_trees(
+            'relaxed-binary-simple.time.trees.gz',
+            burnin=10000, sample=1000, detranslate=True)
+        args.writer.add_posterior(posterior, self.metadata, args.log)
 
         args.writer.add_data(
             self.raw_dir.read_nexus('Chapacuran_Swadesh207-2019-labelled.nex'),
